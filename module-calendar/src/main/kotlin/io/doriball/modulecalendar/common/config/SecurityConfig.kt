4fc52b40db8e1a5d18eef024a554002e6b5f6e43
mrcocoball <gunmaru93@gmail.com>
2026-01-06 09:17:22 +0900

    feat: 서비스 API CORS 허용 설정 추가

---
+++ module-calendar/src/main/kotlin/io/doriball/modulecalendar/common/config/SecurityConfig.kt
@@ -0,0 +1,33 @@
+package io.doriball.modulecalendar.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        httpSecurity.cors {configurer -> configurer.configurationSource(corsConfigurationSource())}
+        return httpSecurity.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfigurationSource = CorsConfiguration().apply {
+            allowedOrigins = listOf("http://localhost:3000", "http://localhost:8080")
+            allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfigurationSource)
+        return source
+    }
+
+}
