c889568c440f470ea985285ccdde0769de1f0033
mrcocoball <gunmaru93@gmail.com>
2026-01-06 13:49:37 +0900

    chore: 불필요 import 삭제

--- module-infrastructure/src/main/kotlin/io/doriball/moduleinfrastructure/persistence/entity/StoreEventRuleDocument.kt
+++ module-infrastructure/src/main/kotlin/io/doriball/moduleinfrastructure/persistence/entity/StoreEventRuleDocument.kt
@@ -3,7 +3,6 @@ package io.doriball.moduleinfrastructure.persistence.entity
 import io.doriball.modulecore.enums.DayOfWeekType
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Document(collection = "store_event_rule")
