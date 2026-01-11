fa44a3b3b522bb9092cf154c7dc18201e7a6d9d7
mrcocoball <gunmaru93@gmail.com>
2026-01-11 23:16:12 +0900

    chore: 어드민 CORS 수정

--- module-admin/src/main/kotlin/io/doriball/moduleadmin/auth/adapter/out/security/config/SecurityConfig.kt
+++ module-admin/src/main/kotlin/io/doriball/moduleadmin/auth/adapter/out/security/config/SecurityConfig.kt
@@ -15,6 +15,9 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @EnableMethodSecurity(securedEnabled = true)
 @Configuration
@@ -45,6 +48,7 @@ class SecurityConfig(val customUserDetailsService: CustomUserDetailsService) {
                     .invalidateHttpSession(true)
                     .deleteCookies("JSESSIONID")
             }
+            .cors { configurer -> configurer.configurationSource(corsConfigurationSource()) }
 
         return httpSecurity.build()
     }
@@ -60,5 +64,20 @@ class SecurityConfig(val customUserDetailsService: CustomUserDetailsService) {
 
     @Bean
     fun logoutSuccessHandler(): LogoutSuccessHandler = CustomLogoutSuccessHandler()
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfigurationSource = CorsConfiguration().apply {
+            allowedOrigins = listOf(
+                "http://dev-admin.doriball.com:9427"
+            )
+            allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfigurationSource)
+        return source
+    }
 
 }
