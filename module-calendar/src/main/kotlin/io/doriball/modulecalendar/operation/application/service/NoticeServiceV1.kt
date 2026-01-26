7c27ff140d24e69adb211908336204feb677e307
mrcocoball <gunmaru93@gmail.com>
2026-01-26 14:04:58 +0900

    feat: 캐시 적용

--- module-calendar/src/main/kotlin/io/doriball/modulecalendar/operation/application/service/NoticeServiceV1.kt
+++ module-calendar/src/main/kotlin/io/doriball/modulecalendar/operation/application/service/NoticeServiceV1.kt
@@ -2,21 +2,26 @@ package io.doriball.modulecalendar.operation.application.service
 
 import io.doriball.modulecalendar.operation.application.dto.NoticeDetailDto
 import io.doriball.modulecalendar.operation.application.dto.NoticeDto
+import io.doriball.modulecalendar.operation.application.dto.NoticePageDto
 import io.doriball.modulecalendar.operation.application.port.`in`.NoticeUseCase
 import io.doriball.modulecalendar.operation.application.port.`in`.dto.ReadNoticeDetailCommand
 import io.doriball.modulecalendar.operation.application.port.`in`.dto.ReadNoticesCommand
 import io.doriball.modulecalendar.operation.application.port.out.NoticePort
+import io.doriball.modulecore.shared.codes.SharedCacheName
 import io.doriball.modulecore.shared.exception.NotFoundException
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class NoticeServiceV1(val noticePort: NoticePort) : NoticeUseCase {
 
-    override fun getNotices(command: ReadNoticesCommand): Pair<List<NoticeDto>, Long> {
+    @Cacheable(value = [SharedCacheName.NOTICES], key = "(#command.page ?: '1') + '_' + (#command.size ?: '10')")
+    override fun getNotices(command: ReadNoticesCommand): NoticePageDto {
         val (notices, total) = noticePort.getNotices(command.page, command.size)
-        return notices.map { NoticeDto.from(it) } to total
+        return NoticePageDto(notices = notices.map { NoticeDto.from(it) }, totalSize = total)
     }
 
+    @Cacheable(value = [SharedCacheName.NOTICE_DETAIL], key = "#command.noticeId")
     override fun getNoticeDetail(command: ReadNoticeDetailCommand): NoticeDetailDto {
         val notice = noticePort.getNoticeDetail(command.noticeId) ?: throw NotFoundException()
         return NoticeDetailDto.from(notice)
