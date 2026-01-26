b91f89d99a2317b4fdf9078d2137311aa88278cc
mrcocoball <gunmaru93@gmail.com>
2026-01-26 14:04:27 +0900

    feat: 코어 모듈 내 공용 캐시 이름 object class 추가

---
+++ module-core/src/main/kotlin/io/doriball/modulecore/shared/codes/SharedCacheName.kt
@@ -0,0 +1,12 @@
+package io.doriball.modulecore.shared.codes
+
+object SharedCacheName {
+
+    const val REGIONS = "regions"
+    const val EVENTS = "events"
+    const val STORES = "stores"
+    const val STORE_DETAIL = "store_detail"
+    const val NOTICES = "notices"
+    const val NOTICE_DETAIL = "notice_detail"
+
+}
